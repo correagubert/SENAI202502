f150c80db0644792ac993e398682fc792392f603
NICOLAS CORRÊA GUBERT <nicolas_gubert@estudante.sesisenai.org.br>
2025-10-07 20:58:32 -0300

    Novo PC.

---
+++ aula01/clinica/src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+// import react router
+
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
+
+
+import './index.css'
+import Login from './pages/Login/Login'
+import { AuthProvider } from './contexts/AuthContext'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+// import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </AuthProvider>
+    {/* <App /> */}
+  </StrictMode>,
+)
